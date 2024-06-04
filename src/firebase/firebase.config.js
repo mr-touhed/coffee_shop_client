23bc324b528c53fbf72ed0e5d1d19d503601c4bc
Touhed <mr.touhed.i@outlook.com>
2024-06-04 19:43:05 +0600

    remove filebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVP4HRj7tikP_rMZuPRfRMH9D8eu1shgQ",
-  authDomain: "super-shop-fccee.firebaseapp.com",
-  projectId: "super-shop-fccee",
-  storageBucket: "super-shop-fccee.appspot.com",
-  messagingSenderId: "465115661661",
-  appId: "1:465115661661:web:12bf711cfc139fc742d4b4"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
